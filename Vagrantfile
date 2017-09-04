f2906b8eedee4ba53b79209c36a658844651bf00
Gionne Cannister <gionnec@gmail.com>
2017-09-03 21:49:58 -0400

    Adds port forwarding for mysql to Vagrantfile. Adds configuration to setup.sh for mysql remote connection and create wp database.

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,7 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 80, host: 8888
+  config.vm.network "forwarded_port", guest: 3306, host: 3307
   config.vm.provision :shell, path: "setup.sh"
   config.vm.synced_folder "./", "/var/www/html"
 end
--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password abc
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password abcd1234'
 apt-get update
 apt-get install -y apache2 mysql-server php libapache2-mod-php php-mcrypt php-mysql
+
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'abcd1234';FLUSH PRIVILEGES;CREATE DATABASE wp;" | mysql -uroot -pabcd1234
+cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
+service apache2 restart
